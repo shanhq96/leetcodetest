36b75d9d0b63f54b285610b920d75c557736d180
shanhq96 <shanhq96@gmail.com>
2017-03-27 09:39:46 +0800

    leetcode137 SingleNumberII 若重复出现3次,则进行二进制按位求和模3即为所求 特别注意负数的情况 因为负数在计算机中存储补码

--- leetcode137_Single Number II/src/Solution.java
+++ leetcode137_Single Number II/src/Solution.java
@@ -14,21 +14,30 @@ public class Solution {
     public int singleNumber(int[] nums) {
         int[] bits = new int[32];
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0, test = 1; j < 32; j++,test = test << 1) {
-                bits[j] += (nums[i] & test)/test;//将目标数字的每一位与该位为1的二进制数做与,若除测试数字后为1,则该位为1,否则为0;
+            for (int j = 0, test = 1; j < 32; j++, test = test << 1) {
+                bits[j] += (nums[i] & test) / test;//将目标数字的每一位与该位为1的二进制数做与,若除测试数字后为1,则该位为1,否则为0;
                 //如 3(10)=11(2)    11(2)&10(2)=10(2)=2(10)   2/2=1
             }
         }
-        String result = "";
-        for (int j = 0; j < 32; j++) {
-            result = (bits[j] % 3) + result;
+        String temp = "";
+        int result = 0;
+        if ((bits[31]%3)==0){//正数
+            for (int j = 0; j < 31; j++) {
+                temp = (bits[j] % 3) +temp;
+            }
+            result = Integer.valueOf(temp,2);
+        }else {//负数
+            for (int j = 0; j < 31; j++) {
+                temp = ((bits[j] % 3)^1) +temp;
+            }
+            result = -(Integer.valueOf(temp,2) + 1);
         }
-        return Integer.valueOf(result, 2);
+        return result;
     }
 
     @Test
     public void testSingleNumber() {
-        int[] nums = {1, 1, 2, 2, 1, 2, 3};
-        Assertions.assertEquals(3, singleNumber(nums));
+        int[] nums = {1};
+        Assertions.assertEquals(1, singleNumber(nums));
     }
 }
